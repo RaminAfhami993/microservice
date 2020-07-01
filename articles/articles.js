ab01b65bda6309ba64db8ad7cc2a4e2e79e0e2c7
Ramin <afhami.ramin@yahoo.com>
2020-07-01 11:56:34 +0430

    add add article and get atrticles end points

--- articles/articles.js
+++ articles/articles.js
@@ -2,15 +2,33 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ramin:12345678asd@cluster0.t8pmx.mongodb.net/articleService?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+mongoose.connect('mongodb://localhost:27017/articleService', {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Connected to articleService DB");   
 });
 
+app.use(express.json());
+
 const Article = require('./article');
 
-app.get("/", (req, res) => {
-    res.send("test");
+app.post('/article', (req, res) => {
+    new Article({
+        title: req.body.title,
+        description: req.body.description
+    }).save((err, article) => {
+        console.log(err);
+        
+        if (err) return res.send('err');
+        else return res.json(article)
+    });
 });
+
+app.get('/articles', (req, res) => {
+    Article.find().then(articles => {
+        return res.json(articles)
+    }).catch(err => {
+        return res.send('err');
+    })
+})
 
 app.listen(7777, () => {
     console.log("Up and running --- articles ms");
